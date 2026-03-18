9c34279c2c1395d304a222d9c50aaa9f5398e3bb
Maurício de Oliveira Santos Rodrigues <mauriciosts08@gmail.com>
2026-03-17 22:15:56 -0300

    Aula 02 a 05

---
+++ C# e .NET/Aula02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Aula 02 - Variáveis
+
+namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Tipos em C#
+        String name = "Maurício";
+        int age = 21;
+        double heigt = 1.88;
+        char gender = 'M';
+        bool isRunning = true;
+
+        //Testando os tipos
+        Console.WriteLine("Programa está rodando? " + isRunning);
+        Console.WriteLine("Nome: " + name + "\nIdade: " + age + "\nAltura: " + heigt + "\nGênero: " + gender);
+    }
+}
---
+++ C# e .NET/Aula05/Program.cs
@@ -0,0 +1,30 @@
+﻿// Aula 05 - Operadores Aritméticos
+
+namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("========== Calculadora Simples =========");
+            Console.WriteLine("Digite o primeiro número: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int subtraction = number1 - number2;
+            int multiplication = number1 * number2;
+            int division = number1 / number2;
+            int module = number1 % number2;
+
+            Console.WriteLine("O valor da soma é: " + sum);
+            Console.WriteLine("O valor da subtração é: " + subtraction);
+            Console.WriteLine("O valor da multiplicação é: " + multiplication);
+            Console.WriteLine("O valor da divisão é: " + division);
+            Console.WriteLine("O resto da divisão é: " + module);
+            Console.WriteLine("========================================");
+        }
+    }
+}
